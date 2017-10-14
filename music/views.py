d473dbdddc6ac872be765ff6d3e1b2c21504b2ef
duckied <twebing@yahoo.com>
2017-10-14 02:09:07 -0500

    Initial commit

---
+++ music/views.py
@@ -0,0 +1,16 @@
+from django.views import generic
+from .models import Album
+from lib2to3.fixes.fix_input import context
+
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+    
+    def get_queryset(self):
+        return Album.objects.all()
+    
+    
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
